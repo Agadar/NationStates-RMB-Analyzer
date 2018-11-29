04ba82b47d26053b24c0f286cb78f2ea16086535
Agadar <martin.donkersloot@outlook.com>
2018-11-29 13:53:31 +0100

    Moved public method above private methods.

--- src/main/java/com/github/agadar/nsregionalrankings/RmbStatisticsGenerator.java
+++ src/main/java/com/github/agadar/nsregionalrankings/RmbStatisticsGenerator.java
@@ -28,6 +28,55 @@ public final class RmbStatisticsGenerator {
     }
 
     /**
+     * Generates a report containing the rankings of the nations with: - the most
+     * RMB post likes received; - the most RMB post likes given; - the most RMB
+     * posts made; - the most likes per RMB post on average; - the most endorsements
+     * given. in the specified region in between the specified dates.
+     *
+     * If any exception is caught at all, then the returned report contains the
+     * error message.
+     *
+     * @param region     The region to generate a report of.
+     * @param maxResults The maximum number of results per list to return. Always at
+     *                   least 1.
+     * @param epochStart The lower bound of the time frame to report on. If 0, then
+     *                   there is no lower bound.
+     * @param epochEnd   The upper bound of the time frame to report on. If 0, then
+     *                   there is no upper bound.
+     * @return The generated report.
+     */
+    public String generateReport(String region, int maxResults, long epochStart, long epochEnd) {
+        if (region == null || region.isEmpty()) {
+            return "No region specified!";
+        }
+        if (maxResults < 1) {
+            maxResults = 1;
+        }
+        var regionQuery = nationStates.getRegion(region).shards(RegionShard.MOST_LIKED, RegionShard.MOST_LIKES,
+                RegionShard.MOST_POSTS);
+
+        if (epochStart > 0) {
+            regionQuery.postsFrom(epochStart);
+        }
+        if (epochEnd > 0) {
+            regionQuery.postsTo(epochEnd);
+        }
+
+        Region regionQueryResult;
+        try {
+            regionQueryResult = regionQuery.execute();
+        } catch (Exception ex) {
+            return ex.getMessage();
+        }
+
+        String toReturn = mostLikesReceived(regionQueryResult, maxResults);
+        toReturn += mostLikesGiven(regionQueryResult, maxResults);
+        toReturn += mostPosts(regionQueryResult, maxResults);
+        toReturn += mostLikesPerPost(regionQueryResult, maxResults);
+        return String.format(toReturn);
+    }
+
+    /**
      * Generates a report containing the rankings of the nations with the most RMB
      * post likes received.
      *
@@ -123,54 +172,5 @@ public final class RmbStatisticsGenerator {
 
     private String wrapInNationTags(String nationName) {
         return String.format("[nation]%s[/nation]", nationName);
-    }
-
-    /**
-     * Generates a report containing the rankings of the nations with: - the most
-     * RMB post likes received; - the most RMB post likes given; - the most RMB
-     * posts made; - the most likes per RMB post on average; - the most endorsements
-     * given. in the specified region in between the specified dates.
-     *
-     * If any exception is caught at all, then the returned report contains the
-     * error message.
-     *
-     * @param region     The region to generate a report of.
-     * @param maxResults The maximum number of results per list to return. Always at
-     *                   least 1.
-     * @param epochStart The lower bound of the time frame to report on. If 0, then
-     *                   there is no lower bound.
-     * @param epochEnd   The upper bound of the time frame to report on. If 0, then
-     *                   there is no upper bound.
-     * @return The generated report.
-     */
-    public String generateReport(String region, int maxResults, long epochStart, long epochEnd) {
-        if (region == null || region.isEmpty()) {
-            return "No region specified!";
-        }
-        if (maxResults < 1) {
-            maxResults = 1;
-        }
-        var regionQuery = nationStates.getRegion(region).shards(RegionShard.MOST_LIKED, RegionShard.MOST_LIKES,
-                RegionShard.MOST_POSTS);
-
-        if (epochStart > 0) {
-            regionQuery.postsFrom(epochStart);
-        }
-        if (epochEnd > 0) {
-            regionQuery.postsTo(epochEnd);
-        }
-
-        Region regionQueryResult;
-        try {
-            regionQueryResult = regionQuery.execute();
-        } catch (Exception ex) {
-            return ex.getMessage();
-        }
-
-        String toReturn = mostLikesReceived(regionQueryResult, maxResults);
-        toReturn += mostLikesGiven(regionQueryResult, maxResults);
-        toReturn += mostPosts(regionQueryResult, maxResults);
-        toReturn += mostLikesPerPost(regionQueryResult, maxResults);
-        return String.format(toReturn);
     }
 }
